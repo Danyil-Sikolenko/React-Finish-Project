57ff86a3111def6546606ac61ec14991c9201382
Danyil-Sikolenko <banielsikolenko@gmail.com>
2025-06-02 14:02:26 +0200

    Project structure ready, installed frontend stack

---
+++ frontend/src/App.jsx
@@ -0,0 +1,31 @@
+import { createBrowserRouter,  } from "react-router";
+import Layout from "./components/Layout";
+import { RouterProvider } from 'react-router'
+import './App.css'
+
+
+
+
+ const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, element: <div>Hello World</div> },
+    ]
+  },
+]);
+
+
+
+
+function App() {
+
+  return (
+    <RouterProvider router={router}>
+
+    </RouterProvider>
+  )
+}
+
+export default App
